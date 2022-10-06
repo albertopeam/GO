bc679c1c352de229811652f5875de65232eb20b8
Alberto Penas Amor <alberto__pa@hotmail.com>
2022-10-06 23:34:25 +0200

    updated auth-get-grants

--- cosmoshub/authz-get-grants/main.go
+++ cosmoshub/authz-get-grants/main.go
@@ -4,16 +4,19 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/authz"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	// Create GRPC connection
-	mainNet := "54.180.225.240:9090" // cosmos mainnet
+	mainNet := "rpc.sentry-01.theta-testnet.polypore.xyz:9090" // testnet // "54.180.225.240:9090" cosmos mainnet
 	grpcConn, err := grpc.Dial(
 		mainNet,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
@@ -24,16 +27,31 @@ func main() {
 	}
 	defer grpcConn.Close()
 
+	// addresses
+	granter := "cosmos19kzdcmysekqu926fwdcjg5pdqlx3saujcldys5"
+	grantee := "cosmos1c28cfmvvne62n5347h3nptar7ka0dffxc0nd8z"
+	validator := "cosmosvaloper1c28cfmvvne62n5347h3nptar7ka0dffxam8ct3"
+
+	// Check granted Authorizations
+	checkGrants(grpcConn, granter)
+
+	// Enable grant(if already granted it will be overwritten)
+	grant(grpcConn, granter, grantee, validator)
+
+	// Check granted Authorizations(again)
+	checkGrants(grpcConn, granter)
+}
+
+func checkGrants(grpcConn *grpc.ClientConn, address string) {
 	// Create the query client
-	authzClient := authz.NewQueryClient(grpcConn)
+	queryClient := authz.NewQueryClient(grpcConn)
 
 	// Create the query
-	address := "cosmos196ax4vc0lwpxndu9dyhvca7jhxp70rmcfhxsrt"
 	request := authz.QueryGranterGrantsRequest{Granter: address}
 	fmt.Println("QueryGrantsRequest", request)
 
 	// Query grants
-	response, err := authzClient.GranterGrants(context.Background(), &request)
+	response, err := queryClient.GranterGrants(context.Background(), &request)
 	if err != nil {
 		log.Fatalf("GranterGrants Error %s", err)
 	}
@@ -46,4 +64,56 @@ func main() {
 			fmt.Println("Grant", grant.Expiration, grant.Authorization.GetTypeUrl(), grant.Granter, grant.Grantee)
 		}
 	}
+}
+
+func grant(grpcConn *grpc.ClientConn, granter string, grantee string, validator string) {
+	// Create the MsgClient
+	msgClient := authz.NewMsgClient(grpcConn)
+
+	// Create the MsgGrant
+	var tokens *types.Coin // empty means no limit https://pkg.go.dev/github.com/cosmos/cosmos-sdk/types#Coin
+	valAddress, err := types.ValAddressFromBech32(validator)
+	if err != nil {
+		log.Fatalf("ValAddressFromBech32 Error %s", err)
+	}
+	allowed := []types.ValAddress{valAddress}
+	denied := []types.ValAddress{}
+	stakeAuth, err := stakingtypes.NewStakeAuthorization(allowed, denied, stakingtypes.AuthorizationType_AUTHORIZATION_TYPE_REDELEGATE, tokens) // https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/staking/types#NewStakeAuthorization https://github.com/cosmos/cosmos-sdk/blob/v0.46.0-rc1/x/staking/types/authz.go#L15-L35
+	if err != nil {
+		log.Fatalf("stakingtypes.NewStakeAuthorization Error %s", err)
+	}
+	granterAddr, err := types.AccAddressFromBech32(granter)
+	if err != nil {
+		log.Fatalf("GranterAddr Error %s", err)
+	}
+	granteeAddr, err := types.AccAddressFromBech32(grantee)
+	if err != nil {
+		log.Fatalf("GranteeAddr Error %s", err)
+	}
+	expiration := time.Now().Add(time.Hour * 24 * 30)
+	msgGrant, err := authz.NewMsgGrant(granterAddr, granteeAddr, stakeAuth, &expiration) // https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#NewMsgGrant https://github.com/cosmos/cosmos-sdk/blob/v0.46.1/x/authz/spec/03_messages.md#msggrant
+	if err != nil {
+		log.Fatalf("NewMsgGrant Error %s", err)
+	}
+
+	// Print data
+	fmt.Println("Tokens", tokens)
+	fmt.Println("Expiration", expiration)
+	fmt.Println("Allowed addresses", valAddress.String())
+	fmt.Println("Denied addresses", denied)
+	fmt.Println("Granter address", granterAddr.String())
+	fmt.Println("Grantee address", granteeAddr.String())
+
+	// Run Grant
+	//TODO: review discord https://discord.com/channels/669268347736686612/1019978171367559208
+	//TODO: is needed to have stake before(I don't think, only a grant).
+	//TODO: val address is needed? or is needed the default address?
+	//TODO: is not impl in testnet?
+	response, err := msgClient.Grant(context.Background(), msgGrant, grpc.FailFastCallOption{FailFast: false})
+	if err != nil {
+		log.Fatalf("MsgClient.Grant Error %s", err)
+	}
+
+	// Print the response(empty struct if success)
+	fmt.Println("Grant success", response)
 }
--- cosmoshub/authz-get-grants/readme.md
+++ cosmoshub/authz-get-grants/readme.md
@@ -16,14 +16,42 @@
 
 [Stake authorization type](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/staking/types#StakeAuthorization)
 
-## Steps to grant to the granter a stake redelegation authorization
+## Cosmos testnet info
+
+[Cosmos public testnet](https://github.com/cosmos/testnets/tree/master/public)
+
+## Steps to check granter grants
 
 1. Create [grpc connection](https://pkg.go.dev/google.golang.org/grpc#ClientConn)
 
-2. Create the [query client](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#NewQueryClient) for the module. [Query client interface](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#QueryClient)
+2. Create the [query client](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#NewQueryClient) for the module. [QueryClient interface](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#QueryClient)
 
 3. Create the [QueryGranterGrantsRequest](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#QueryGranterGrantsRequest) to obtain a list of GrantAuthorization
 
 4. Invoke `GranterGrants` for [QueryClient](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#QueryClient)
 
 5. Print [Grants](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#Grant): Auth+Expiration if success. Cosmos SDK Built-in Authorizations: [`GenericAuthorization`](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#GenericAuthorization), `SendAuthorization` and [`StakeAuthorization`](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/staking/types#StakeAuthorization)
+
+## Steps to grant to the grantee a stake redelegation authorization from the granter
+
+Before start we need to find a validator to be the grantee. Check the testnet explorers to do that:
+
+* [Testnet github doc](https://github.com/cosmos/testnets/tree/master/public) 
+
+* [Testnet explorer big dipper](https://explorer.theta-testnet.polypore.xyz/) | [Testnet explorer mintscan](https://cosmoshub-testnet.mintscan.io/cosmoshub-testnet) | [Testnet explorer big dipper live](https://testnet.cosmos.bigdipper.live/)
+
+* [Testnet validators big dipper](https://explorer.theta-testnet.polypore.xyz/validators). 
+
+    We pick [stakely.io](https://explorer.theta-testnet.polypore.xyz/validator/cosmosvaloper1c28cfmvvne62n5347h3nptar7ka0dffxam8ct3) validator, so `grantee` will be `cosmos1c28cfmvvne62n5347h3nptar7ka0dffxc0nd8z`(USE AUTODELEGATION ADDRESS)
+
+Grant steps:
+
+1. Create [grpc connection](https://pkg.go.dev/google.golang.org/grpc#ClientConn)
+
+2. Create the [MsgClient](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#NewMsgClient) to grant/revoke authorizations. [MsgClient interface](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#MsgClient)
+
+3. Create the [Grant](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#NewGrant) and [MsgGrant](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#MsgGrant) data
+
+4. Run the [Grant via MsgClient](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#MsgClient)
+
+5. Print [MsgGrantResponse](https://pkg.go.dev/github.com/cosmos/cosmos-sdk/x/authz#MsgGrantResponse)
